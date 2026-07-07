cbe39733c857f21b223a08a13384a0c56aa5b6cd
PanelCT <fabiogr49@gmail.com>
2026-07-07 15:23:40 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+const SUPABASE_URL = "INSERISCI_URL_SUPABASE";
+const SUPABASE_KEY = "INSERISCI_CHIAVE_SUPABASE";
+
+const client = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+document.querySelector("button").addEventListener("click", async () => {
+
+const dati = {
+  codice_campione: document.getElementById("campione").value,
+  nome: document.getElementById("nome").value,
+  cognome: document.getElementById("cognome").value,
+
+  fruttato: document.getElementById("fruttato").value,
+  amaro: document.getElementById("amaro").value,
+  piccante: document.getElementById("piccante").value,
+
+  riscaldo: document.getElementById("riscaldo").value,
+  muffa_umidita: document.getElementById("muffa").value,
+  morchia: document.getElementById("morchia").value,
+  avvinato_inacetito: document.getElementById("avvinato").value,
+  metallico: document.getElementById("metallico").value,
+  rancido: document.getElementById("rancido").value,
+
+  note: document.getElementById("note").value
+};
+
+const { error } = await client
+.from("degustazioni")
+.insert([dati]);
+
+if(error){
+ alert("Errore: " + error.message);
+}else{
+ alert("Scheda inviata correttamente!");
+}
+
+});
