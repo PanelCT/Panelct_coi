ba6549eeebab9ecf247c68a9be60b724a7af5443
PanelCT <fabiogr49@gmail.com>
2026-07-10 10:38:38 +0200

    capo_panel.js

--- capo_panel.js
+++ capo_panel.js
@@ -1,10 +1,10 @@
-alert("Capo Panel funzionante");
-
 document.getElementById("nuovaSessione").onclick = function() {
+
   let id = "PANEL-" + new Date().toISOString().slice(0,10);
 
   document.getElementById("sessioneCorrente").innerHTML =
     "Sessione attiva: " + id;
 
   localStorage.setItem("sessioneCorrente", id);
-}; 
+
+};
