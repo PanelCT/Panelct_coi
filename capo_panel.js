21ab932a28dd3e283b947e703e44cb14b2487894
PanelCT <fabiogr49@gmail.com>
2026-07-10 10:49:06 +0200

    capo_panel.js

--- capo_panel.js
+++ capo_panel.js
@@ -1,10 +1,29 @@
-document.getElementById("nuovaSessione").onclick = function() {
+const SUPABASE_URL = "https://wxgkjaobfvwwcgywtwfe.supabase.co";
+const SUPABASE_KEY = "INSERISCI_LA_TUA_CHIAVE";
 
-  let id = "PANEL-" + new Date().toISOString().slice(0,10);
+const client = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+document.getElementById("nuovaSessione").onclick = async function() {
+
+  let codice = "PANEL-" + new Date().toISOString().slice(0,10);
+
+  const { error } = await client
+    .from("sessioni")
+    .insert([
+      {
+        codice_sessione: codice,
+        attiva: true
+      }
+    ]);
+
+  if (error) {
+    alert("Errore: " + error.message);
+    return;
+  }
 
   document.getElementById("sessioneCorrente").innerHTML =
-    "Sessione attiva: " + id;
-
-  localStorage.setItem("sessioneCorrente", id);
-
+    "Sessione attiva: " + codice;
 };
